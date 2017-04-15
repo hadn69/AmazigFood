275c1bc1866b4584a7085be6383893861b9391ab
AmazigJj <theamazigjj@gmail.com>
2017-04-15 19:58:53 +0100

    trying to fix the missing texture...

--- src/main/java/amazigJj/AmazigFood/init/ModItems.java
+++ src/main/java/amazigJj/AmazigFood/init/ModItems.java
@@ -1,9 +1,9 @@
 package amazigJj.AmazigFood.init;
 
 import amazigJj.AmazigFood.items.ItemCherry;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModItems {
@@ -23,6 +23,6 @@ public class ModItems {
 	}
 	
 	private static void registerRender(Item item) {
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(item.getUnlocalizedName().substring(5), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
 	}
 }
