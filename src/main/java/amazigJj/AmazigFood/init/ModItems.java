d0af4c68ab55865c503c7aa422fc906859168631
AmazigJj <theamazigjj@gmail.com>
2017-04-16 01:31:50 +0100

    changed cherry texture and made it food
    made the cherry texture better (not much better), and also made it into
    an actual food

--- src/main/java/amazigJj/AmazigFood/init/ModItems.java
+++ src/main/java/amazigJj/AmazigFood/init/ModItems.java
@@ -4,7 +4,6 @@ import amazigJj.AmazigFood.items.ItemCherry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModItems {
--- src/main/java/amazigJj/AmazigFood/items/ItemCherry.java
+++ src/main/java/amazigJj/AmazigFood/items/ItemCherry.java
@@ -1,11 +1,11 @@
 package amazigJj.AmazigFood.items;
 
 import amazigJj.AmazigFood.AmazigFood;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 
-public class ItemCherry extends Item {
-	
+public class ItemCherry extends ItemFood {
 	public ItemCherry() {
+		super(2, 0.2F, false);
 		setUnlocalizedName(AmazigFood.AmazigFoodItems.Cherry.getUnlocalizedName());
 		setRegistryName(AmazigFood.AmazigFoodItems.Cherry.getRegistryName());
 	}
