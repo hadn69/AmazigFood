1f795d8a0050de6a074e9b59d425764dff55edbc
AmazigJj <theamazigjj@gmail.com>
2017-04-16 21:09:05 +0100

    installed liblib and started using it

--- src/main/java/amazigJj/AmazigFood/blocks/BlockLogCherry.java
+++ src/main/java/amazigJj/AmazigFood/blocks/BlockLogCherry.java
@@ -1,17 +1,12 @@
 package amazigJj.AmazigFood.blocks;
 
-import amazigJj.AmazigFood.AmazigFood;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
+import com.teamwizardry.librarianlib.features.base.block.BlockModLog;
 
-public class BlockLogCherry extends Block {
+import amazigJj.AmazigFood.AmazigFood;
+
+public class BlockLogCherry extends BlockModLog {
 	public BlockLogCherry() {
-		super(Material.WOOD);
 		setUnlocalizedName(AmazigFood.AmazigFoodBlocks.CherryLog.getUnlocalizedName());
-		setRegistryName(AmazigFood.AmazigFoodBlocks.CherryLog.getUnlocalizedName());
-		
-		this.setHardness(2.0F);
-		this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		setRegistryName(AmazigFood.AmazigFoodBlocks.CherryLog.getRegistryName());
 	}
 }
