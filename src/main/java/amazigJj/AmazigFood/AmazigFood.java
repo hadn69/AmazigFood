3d953b020cc225e4632ae69e2a2fc09c7019df60
AmazigJj <theamazigjj@gmail.com>
2017-05-03 19:04:19 +0100

    added reference
    added reference to make the main class look nicer

--- src/main/java/amazigJj/AmazigFood/AmazigFood.java
+++ src/main/java/amazigJj/AmazigFood/AmazigFood.java
@@ -12,13 +12,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = AmazigFood.MOD_ID, name = AmazigFood.NAME, version = AmazigFood.VERSION, acceptedMinecraftVersions = AmazigFood.ACCEPTED_VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSION)
 public class AmazigFood {
-	
-	public static final String MOD_ID = "amazigfood";
-	public static final String NAME = "AmazigFood";
-	public static final String VERSION = "1.0";
-	public static final String ACCEPTED_VERSION = "[1.11.2]";
 	
 	@Instance
 	public static AmazigFood instance;
@@ -49,48 +44,5 @@ public class AmazigFood {
 		
 	}
 	
-	public static enum AmazigFoodItems {
-		Cherry("cherry", "itemcherry");
-		
-		private String unlocalizedName;
-		private String registryName;
-		
-		AmazigFoodItems(String unlocalizedName, String registryName) {
-			this.unlocalizedName = unlocalizedName;
-			this.registryName = registryName;
-		}
-		
-	
-		
-		public String getUnlocalizedName(){
-			return unlocalizedName;
-		}
-		
-		public String getRegistryName() {
-			return registryName;
-		}
-		
-		
-	}
-	public static enum AmazigFoodBlocks {
-		CherryLog("cherrylog","blocklogcherry"),
-		CherryLeaves("cherryLeaves","blockleavescherry"),
-		CherrySapling("cherrysapling","blocksaplingcherry");
-		
-		private String unlocalizedName;
-		private String registryName;
-		
-		AmazigFoodBlocks(String unlocalizedName, String registryName){
-			this.unlocalizedName = unlocalizedName;
-			this.registryName = registryName;
-		}
-		
-		public String getUnlocalizedName(){
-			return unlocalizedName;
-		}
-		
-		public String getRegistryName() {
-			return registryName;
-		}
-	}
+
 }
--- src/main/java/amazigJj/AmazigFood/items/ItemCherry.java
+++ src/main/java/amazigJj/AmazigFood/items/ItemCherry.java
@@ -1,12 +1,12 @@
 package amazigJj.AmazigFood.items;
 
-import amazigJj.AmazigFood.AmazigFood;
+import amazigJj.AmazigFood.Reference;
 import net.minecraft.item.ItemFood;
 
 public class ItemCherry extends ItemFood {
 	public ItemCherry() {
 		super(2, 0.2F, false);
-		setUnlocalizedName(AmazigFood.AmazigFoodItems.Cherry.getUnlocalizedName());
-		setRegistryName(AmazigFood.AmazigFoodItems.Cherry.getRegistryName());
+		setUnlocalizedName(Reference.AmazigFoodItems.Cherry.getUnlocalizedName());
+		setRegistryName(Reference.AmazigFoodItems.Cherry.getRegistryName());
 	}
 }
