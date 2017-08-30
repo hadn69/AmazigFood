7734933bcd3915888d6c684cacf116ef79bbe479
Kitten <InsomniaKitten@users.noreply.github.com>
2017-08-30 23:39:40 +0100

    Fix all the things
    - Final fields for mod information, added mod name and version
    - Allow mod to run on 1.12.x versions
    - Remove unnecessary static item class

--- src/main/java/amazigJj/AmazigFood.java
+++ src/main/java/amazigJj/AmazigFood.java
@@ -1,9 +1,9 @@
 package amazigJj;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -12,11 +12,19 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid="cherry")
+@Mod(   modid=AmazigFood.MOD_ID,
+        name = AmazigFood.MOD_NAME,
+        version = AmazigFood.VERSION,
+        acceptedMinecraftVersions = AmazigFood.MC_VERSIONS)
 @Mod.EventBusSubscriber
-public class amazigfood{
+public class AmazigFood {
 
-    public static final Item CHERRY = new ItemCherry();
+    public static final String MOD_ID = "cherry", MOD_NAME = "Cherry";
+    public static final String VERSION = "1.0.0", MC_VERSIONS = "[1.12,1.13)";
+
+    public static final Item CHERRY = new ItemFood(1, 0.1f, false)
+            .setRegistryName(AmazigFood.MOD_ID + ":cherry")
+            .setUnlocalizedName(AmazigFood.MOD_ID + ".cherry");
 
     @SubscribeEvent
     public static void onItemRegistry(RegistryEvent.Register<Item> event) {
@@ -25,17 +33,9 @@ public class amazigfood{
 
     @SubscribeEvent @SideOnly(Side.CLIENT)
     public static void onModelRegistry(ModelRegistryEvent event) {
-        ModelLoader.setCustomModelResourceLocation(CHERRY, 0, new ModelResourceLocation(CHERRY.getRegistryName(), "inventory"));
+        ResourceLocation name = CHERRY.getRegistryName();
+        ModelResourceLocation mrl = new ModelResourceLocation(name, "inventory");
+        ModelLoader.setCustomModelResourceLocation(CHERRY, 0, mrl);
     }
 
-    public static class ItemCherry extends ItemFood {
-
-        public ItemCherry() {
-            super(1, 1.0f, false);
-            setRegistryName("cherry");
-            setUnlocalizedName("cherry.cherry");
-            setCreativeTab(CreativeTabs.FOOD);
-        }
-
-    }
 }
